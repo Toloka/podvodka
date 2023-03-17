acf844d3b0eed7aff2604b5595bf7cd3b5787544
Nikita Pavlichenko <pilot7747@gmail.com>
2023-03-17 10:35:14 +0000

    Use GPT-2 Large

--- gpt-2-self-supervised-prompt-writing/run_training.sh
+++ gpt-2-self-supervised-prompt-writing/run_training.sh
@@ -1,6 +1,1 @@
-git clone https://github.com/huggingface/transformers.git
-cp -r transformers/examples/pytorch/language-modeling ./
-python3 -m pip install evaluate
-python3 -m pip install -r language-modeling/requirements.txt
-python3 -m pip install git+https://github.com/huggingface/transformers
-python3 language-modeling/run_clm.py --model_name_or_path gpt2 --train_file train_strings.txt --validation_file val_strings.txt --per_device_train_batch_size 8 --per_device_eval_batch_size 8 --learning_rate 5e-04 --preprocessing_num_workers 4 --do_train --do_eval --logging_dir gpt2-finetuned-log --output_dir gpt2-finetuned --num_train_epochs 5 --logging_steps 5 --save_steps 50 --save_total_limit 10 --evaluation_strategy steps --eval_steps 50
+python language-modeling/run_clm.py --model_name_or_path gpt2-large --train_file train_strings_2.txt --validation_file val_strings_2.txt --per_device_train_batch_size 4 --per_device_eval_batch_size 4 --learning_rate 0.00002294 --preprocessing_num_workers 4 --do_train --do_eval --logging_dir gpt2-large-finetuned-log --output_dir gpt2-large-finetuned --num_train_epochs 6 --logging_steps 5 --save_steps 50 --save_total_limit 2 --evaluation_strategy steps --eval_steps 100 --report_to wandb --weight_decay 0.00002092
